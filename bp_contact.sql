00ef4f051e79df459552f366d3265afc3819ce32
Andy Utomo <andyut@outlook.com>
2023-05-29 10:40:21 +0700

    update bp sql

--- bp_contact.sql
+++ bp_contact.sql
@@ -9,8 +9,10 @@ select a.cntctcode AS [Key],
        B.CardFName ,
        b.lictradnum,
        B.U_AR_Person ,
-       B.ShipToDef ,
+       B.ShipToDef ,  
+       a.u_igu_noktp,
+       a.U_igu_nonpwp,
        a.U_blacklist
  from  DBO.OCPR a
 INNER JOIN  DBO.OCRD B ON A.CARDCODE = B.CARDCODE 
- 
+where b.U_blacklist ='Y'
